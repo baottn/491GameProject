17a99869d8ed441cd9a37392f6cbe04fba2f8204
Dilnoza Saidova <73456940+saidodil@users.noreply.github.com>
2023-01-18 12:53:21 -0800

    Added to Toan's code

--- gameengine.js
+++ gameengine.js
@@ -114,6 +114,7 @@ class GameEngine {
         for (let i = this.entities.length - 1; i >= 0; i--) {
             this.entities[i].draw(this.ctx, this);
         }
+        this.camera.draw(this.ctx);
     };
 
     update() {
@@ -127,6 +128,7 @@ class GameEngine {
                 entity.update();
             }
         }
+        this.camera.update();
 
         for (let i = this.entities.length - 1; i >= 0; --i) {
             if (this.entities[i].removeFromWorld) {
