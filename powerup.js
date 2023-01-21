6e5a38d42adf24e6e53f34e1def77cad36fee4df
baottn <nttb04121999@gmail.com>
2023-01-21 14:02:49 -0800

    Update bullet and powerup

--- powerup.js
+++ powerup.js
@@ -1,12 +1,28 @@
 class Powerup{
+    static POWERUP_HEIGHT = 10;
+    static POWERUP_RADIUS = 35;
     constructor(game) {
+        
         this.game = game;
 
-        this.x = Track.getXStart() + 10;
-        this.y = Track.getYStart() + 10;
+        this.x = Track.XStart() + POWERUP_HEIGHT;
+        this.y = Track.YStart() + POWERUP_HEIGHT;
+        this.radius = POWERUP_RADIUS;
+        this.collision = false;
     }
+
+    checkCollisionWithOllie() {
+        let dx = this.x - Ollie.x;
+        let dy = this.y - Ollie.y;
+        let distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.radius + Ollie.radius) {
+            collision = true;
+        }
+    }
+
 
     draw(ctx) {
         ctx.drawImage(ASSET_MANAGER.getAsset("./Powerup.png"),0,0);
-     }
+    }
 }
