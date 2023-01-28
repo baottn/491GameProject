1cd9d9512a5bdc9a2992eda42a3a0aba35e02248
baottn <nttb04121999@gmail.com>
2023-01-28 14:25:13 -0800

    update ollie

--- entities/ollie.js
+++ entities/ollie.js
@@ -294,6 +294,6 @@ class Ollie {
         */
 
         // Draw the animations
-        this.animations[this.index].drawFrame(this.game.clockTick, ctx, this.x, this.y);
+        this.animations[this.index].drawFrame(this.game.clockTick, ctx, this.x, this.y, 5);
     };
 }
