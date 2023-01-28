c84a6508e1758537f5d2329ec2c2f7dd2bda3b37
baottn <nttb04121999@gmail.com>
2023-01-28 10:55:56 -0800

    update animator class

--- Animator.js
+++ Animator.js
@@ -1,20 +1,36 @@
 class Animator {
-    constructor(spritesheet, xStart, yStart, width, height, frameCount, frameDuration) {
-        Object.assign(this, { spritesheet, xStart, yStart, width, height, frameCount, frameDuration });
+    constructor(spritesheet, xStart, yStart, width, height, frameCount, frameDuration, framePadding = 0, reverse = false, loop = true) {
+        Object.assign(this, { spritesheet, xStart, yStart, width, height, frameCount, frameDuration, framePadding, reverse, loop });
 
         this.elapsedTime = 0;
         this.totalTime = frameCount * frameDuration;
     };
 
-    drawFrame(tick, ctx, x, y) {
+    drawFrame(tick, ctx, x, y, scale) {
         this.elapsedTime += tick;
-        const frame = this.currentFrame();
-        if(this.elapsedTime > this.totalTime) this.elapsedTime -= this.totalTime;
+
+        if (this.isDone()) {
+            if (this.loop) {
+                this.elapsedTime -= this.totalTime;
+            } else {
+                return;
+            }
+        }
+
+        let frame = this.currentFrame();
+        if (this.reverse) frame = this.frameCount - frame - 1;
+       
         ctx.drawImage(this.spritesheet,
-            this.xStart + this.width*frame, this.yStart,
+            this.xStart + frame * (this.width + this.framePadding), this.yStart, //source from sheet
             this.width, this.height,
             x, y,
-            this.width*2, this.height*2);
+            this.width * scale,
+            this.height * scale);
+
+        if (PARAMS.DEBUG) {
+            ctx.strokeStyle = 'Green';
+            ctx.strokeRect(x, y, this.width * scale, this.height * scale);
+        }
     };
 
     currentFrame() {
