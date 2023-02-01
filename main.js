496fd61676a18f98de4c6235ed6a8a57034e20cc
baottn <nttb04121999@gmail.com>
2023-02-01 10:15:46 -0800

    recrop and add fire fuel to tank

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const gameEngine = new GameEngine();
 const ASSET_MANAGER = new AssetManager();
 
 ASSET_MANAGER.queueDownload("./img/background.jpg");
-ASSET_MANAGER.queueDownload("./img/tank_body_spritesheet.png");
+ASSET_MANAGER.queueDownload("./img/tank_body_fire.png");
 
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
