96bd3bde5482bcd7f70551330153e90099eef5a8
baottn <nttb04121999@gmail.com>
2023-02-08 14:26:08 -0800

    add power up sprites

--- main.js
+++ main.js
@@ -8,6 +8,8 @@ ASSET_MANAGER.queueDownload("./img/tank_turret.png");
 ASSET_MANAGER.queueDownload("./img/tracks.png");
 ASSET_MANAGER.queueDownload("./img/fire_border.png");  
 ASSET_MANAGER.queueDownload("./img/bullet.png"); 
+ASSET_MANAGER.queueDownload("./img/power_ups.png"); 
+
 
 
 ASSET_MANAGER.downloadAll(() => {
