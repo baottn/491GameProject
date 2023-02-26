62a318dceb36fef10c58a5049c96863e9e098ba5
Engity <ngtoan1107@gmail.com>
2023-02-26 01:30:08 -0800

    Balancing

--- entities/powerup.js
+++ entities/powerup.js
@@ -120,9 +120,9 @@ class Powerup{
 const invincibleSpeedBuff = (player, powerup, game) => {
     //Only replenish the duration speed buff and not stack the effect
     if (!player.invincibility){
-        player.dx *= 5;
+        player.dx *= 3;
     }
-    player.booster = 500;
+    player.booster = 300;
     player.invincibility = true;
     game.camera.score += 25;
 }; 
